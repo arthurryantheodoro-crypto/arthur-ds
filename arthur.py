3ddb5b02ef80f9984128c048a991507163187ccc
arthurryantheodoro-crypto <arthurryantheodoro@gmail.com>
2026-09-01 12:59:34 -0300

    Add files via upload

--- arthur.py
+++ arthur.py
@@ -1,2 +1,8 @@
-for i in range (1,6):
-    print(i*2)
+#codigo identifica se tem permissao para 
+#assistir filmes +16
+idade = int(input("digite sua idade"))
+#so aceita meu numero inteiro
+if idade  >= 16:
+    print("entrada permitida!")
+else:
+    print("entrada nao permitida!")
